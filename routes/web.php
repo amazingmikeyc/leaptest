1aa4793fc1830a43481ac23f0a8295b913c66857
Mike Congreve <mike.congreve@gmail.com>
2026-01-28 14:11:27 +0000

    add notes route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::get('/notes', function () {
+    return Inertia::render('Notes');
+})->name('notes');
