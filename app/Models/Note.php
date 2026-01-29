a8a43f1497ace8dbdbd6c9608e762673fc7b7340
Mike Congreve <mike.congreve@gmail.com>
2026-01-29 20:33:29 +0000

    Create factories and seeder

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Database\Factories\NotesFactory;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[UseFactory(NotesFactory::class)]
 class Note extends Model
 {
+    use HasFactory;
     //primary keys are ON by default as 'id' in database
 
     //timestamps - created_on fields - are created by default.
